a734e7d7494f82cf69e09b3be8b99785eef3eeb7
Salvador <sarevalotamayo@student.rtc.edu>
2024-01-13 20:18:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Numerics;
+
+namespace Assignment_WriteLine_Strings_Salvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string GRUlower = "Gru";
+            string GRUUpper = "GRU";
+            string AGNESLower = "Agnes";
+            string AGNESUpper = "AGNES";
+            string EDITHLower = "Edith";
+            string EDITHUpper = "EDITH";
+            string MARGOLower = "Margo";
+            string MARGOUpper = "MARGO";
+            string VENDORLower = "Vendor";
+            string VENDORUpper = "VENDOR";
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("Oh my gosh look at that fluffy unicorn!");
+            Console.WriteLine("...");
+            Console.WriteLine("He’s so fluffy I’m gonna die!!!");
+
+            Console.WriteLine(MARGOUpper);
+            Console.WriteLine("You gotta let us play for it.");
+
+            Console.WriteLine(GRUUpper);
+            Console.WriteLine("No, no, no.");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("Come on!");
+            Console.WriteLine(GRUlower + "sighs and addresses the slimy" + VENDORLower);
+
+            Console.WriteLine(GRUUpper);
+            Console.WriteLine("How much for the fluffy unicorn?");
+
+            Console.WriteLine(VENDORUpper);
+            Console.WriteLine("(chuckles) Well...it is not for sale. But all you gotta do to win it is knock down that little spaceship there.");
+            Console.WriteLine("The" + VENDORLower + "points to the smallest of the spaceship targets.");
+
+            Console.WriteLine(VENDORUpper);
+            Console.WriteLine("It’s easy!");
+            Console.WriteLine(GRUlower + "nods. He slaps a dollar bill on the table. The vendor slurps on his drink, and presses a button that opens the curtains and readies the cork guns.");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("Yay!");
+            Console.WriteLine("They take aim and open fire! POP! POP! POP! They all miss.");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("Again!");
+
+            Console.WriteLine(MARGOUpper);
+            Console.WriteLine("Wait!");
+
+            Console.WriteLine(EDITHUpper);
+            Console.WriteLine("Ah come on. One more time.");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("Just one more! I accidentally closed my eyes!");
+            Console.WriteLine(GRUlower + "slaps down another bill. POP! POP! POP! This time Agnes's cork hits the target.");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("I hit it, I hit it! Did you see that? I hit it!");
+            Console.WriteLine("The video sign above the game reads: “YOU LOSE." + GRUlower + "reacts");
+
+            Console.WriteLine(EDITHUpper);
+            Console.WriteLine("Awww.");
+
+            Console.WriteLine(GRUUpper);
+            Console.WriteLine("Whoa, whoa, whoa...What was that? She hit that! I saw that with my own eyes!");
+            Console.WriteLine("The Vendor gives" + GRUlower + "a smarmy smile. He flips through a stack of dollar bills.");
+
+            Console.WriteLine(VENDORUpper);
+            Console.WriteLine("Hey, buddy. Let me explain something to you.");
+            Console.WriteLine("The" + VENDORLower + "points to the back of the range.");
+
+            Console.WriteLine(VENDORUpper);
+            Console.WriteLine("Ya see that little tin spaceship? You see how it’s not knocked over? Do you know what that means, professor?");
+            Console.WriteLine("The vendor leans over the counter and gets right in" + GRUlower + "face.");
+
+            Console.WriteLine(VENDORUpper);
+            Console.WriteLine("It means you DON’T GET THE UNICORN! Someone’s got a frowny face. Better luck next time!");
+            Console.WriteLine(GRUlower + "stares at the" + VENDORLower + ", his blood beginning to boil. He remains calm and smiles at the" + VENDORLower + ".");
+
+            Console.WriteLine(GRUUpper);
+            Console.WriteLine("Okay, my turn.");
+            Console.WriteLine(GRUlower + "calmly reaches into his coat and pulls out a small gun that transforms into an enormous RAY GUN.BLAM! A bright blue RAY BLAST connects with the back of the shooting range, leaving no sign of the targets just a giant gaping hole." + GRUlower + "turns to the" + VENDORLower + ".");
+
+            Console.WriteLine(GRUUpper);
+            Console.WriteLine("Knocked over.");
+            Console.WriteLine("The" + VENDORLower + "trembles, silenced." + GRUlower + "pockets his weapon and grabs the unicorn. He hands it to.");
+
+            Console.WriteLine(AGNESUpper+ "," + MARGOUpper + "and" + EDITHUpper);
+            Console.WriteLine("cheer and" + AGNESLower + "hugs the animal tightly.");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("IT’S SO FLUFFY!");
+
+            Console.WriteLine(MARGOUpper);
+            Console.WriteLine("That was awesome!");
+
+            Console.WriteLine(EDITHUpper);
+            Console.WriteLine("You blew up the whole thing!");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("Let’s go destroy another game!");
+
+            Console.WriteLine("And then it happens: actually connecting with the girls and enjoying being with them," + GRUlower + "allows a small smile to creep across his face.");
+
+            Console.WriteLine("THE END");
+        }
+    }
+    
+
+
+}
